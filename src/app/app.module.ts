3482c0be3eccdf20a35baa37549c2053fe86c6c0
truizdiaz <tomiruizdiaz@gmail.com>
2020-11-27 09:37:44 -0300

    2 HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TareasComponent } from './components/tareas/tareas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TareasComponent
   ],
   imports: [
     BrowserModule
